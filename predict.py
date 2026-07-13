540d457e96268696ef05585f761dbf3a43ef7def
Durga Nagendra Kayala <nagendra.kayala95@gmail.com>
2026-07-13 19:52:22 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,7 +4,9 @@ model = joblib.load("alphabet.pkl")
 
 labels = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
+
 def predict_alphabet(img):
+
     pred = model.predict(img)[0]
 
     if isinstance(pred, str):
